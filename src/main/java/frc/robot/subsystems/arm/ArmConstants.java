4c1909c7dcc8354cc0ad5066e4588e5e6d07e1a4
Yo Omura <omura.yo01@gmail.com>
2026-03-25 16:34:19 -0700

    3/25 - Zero'd PID values

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -6,12 +6,12 @@ public class ArmConstants {
   public static final int canId = 5;
   public static final int currentLimit = 50;
 
-  public static final double armkP = 0.05;
+  public static final double armkP = 0.0;
   public static final double armkI = 0.0;
   public static final double armkD = 0.0;
 
-  public static final double zeroAngle = 0;
-  public static final double ninetyAngle = Math.PI / 2;
-  public static final double oneeightyAngle = Math.PI;
-  public static final double twoseventyAngle = Math.PI * 3 / 2;
+  public static final double zeroAngle = 0; // radians
+  public static final double ninetyAngle = Math.PI / 2; // radians
+  public static final double oneeightyAngle = Math.PI; // radians
+  public static final double twoseventyAngle = Math.PI * 3 / 2; // radians
 }
